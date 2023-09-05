a375ffe9f586c9f1bc4d8fc477c909cbc2d41fdb
Rangabread <edhandley479@gmail.com>
2023-09-05 18:02:19 +0800

    Finished

--- static/members_icon_click.js
+++ static/members_icon_click.js
@@ -1,27 +1,26 @@
-//////////////MEMBERS ANIMATION/////////////////////
-// Wait for the DOM to be fully loaded
+
+//This code was created with the help of Chat GPT
 document.addEventListener('DOMContentLoaded', function() {
-  // Add click event to the icon with id 'members-icon'
   document.getElementById('members-icon').addEventListener('click', function() {
-    window.location.href = '/members';  // Redirect to the Members page
+    window.location.href = '/members';  
   });
 
-  // Add click event to the icon with id 'members-icon'
+  
   document.getElementById('team-icon').addEventListener('click', function() {
-    window.location.href = '/team';  // Redirect to the Members page
+    window.location.href = '/team';  
   });
 
   document.getElementById('venue-icon').addEventListener('click', function() {
-    window.location.href = '/venue';  // Redirect to the Members page
+    window.location.href = '/venue';  
   });
   document.getElementById('training-icon').addEventListener('click', function() {
-    window.location.href = '/trainings';  // Redirect to the Members page
+    window.location.href = '/trainings';  
   });
   document.getElementById('dashboard-icon').addEventListener('click', function() {
-    window.location.href = '/dashboard';  // Redirect to the Members page
+    window.location.href = '/dashboard';  
   });
   document.getElementById('sponsor-icon').addEventListener('click', function() {
-    window.location.href = '/sponsor';  // Redirect to the Members page
+    window.location.href = '/sponsor';  
   });
 
 });
